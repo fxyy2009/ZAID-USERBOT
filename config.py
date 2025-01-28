15c766cccce75b2ebb31dca55afd691833c188ef
fxyy2009 <fxyybokep@gmail.com>
2025-01-28 13:47:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6371879956").split()))
+OWNER_ID = int(getenv("OWNER_ID", "6371879956"))
 MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "8114099090:AAEvjgQ-ymC7SauA3FvtsJ4dJZ0DszBOq_w")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
-GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+GIT_TOKEN = getenv("GIT_TOKEN", "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") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
